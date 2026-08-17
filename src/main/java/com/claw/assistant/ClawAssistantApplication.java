7fe82584ff8b07dc7ef7d1aff8ff898a926f4dd3
senhui <2369578398@qq.com>
2026-08-17 10:01:00 +0800

    初始化项目

---
+++ src/main/java/com/claw/assistant/ClawAssistantApplication.java
@@ -0,0 +1,13 @@
+package com.claw.assistant;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClawAssistantApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ClawAssistantApplication.class, args);
+	}
+
+}
